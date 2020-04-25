fa4bb4ece417d14d4e1ec3751cbbebe16b2dab56
lennonchoong <62992865+lennonchoong@users.noreply.github.com>
2020-04-25 20:17:07 +0800

    Update spotify_link.py

--- spotify_link.py
+++ spotify_link.py
@@ -2,8 +2,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from re import findall
 
-cid = "2f03e5a432d94dbdbde91a8b738096e6"
-secret = "419f4a447f4649c6a1c301eb123db602"
+cid = ####
+secret = ####
 
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
